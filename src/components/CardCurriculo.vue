7ab667a3e725699d98899d15d55272a885d7f183
Filipe <filipe.matias@globalbot.com.br>
2022-10-09 23:06:49 -0300

    daily curriculo

--- src/components/CardCurriculo.vue
+++ src/components/CardCurriculo.vue
@@ -1,22 +1,26 @@
 <template>
-    <q-card class="my-card row fixed-center">
+    <q-card class="my-card">
       <q-card-section horizontal>
-        <q-card-section class="">
-          <div class="text-h5">2019 Atual</div>
-          <div class="text-h6">Desenvolvedor full-stack</div>
-          <div class="text-h5">Globalbot</div>
+        <q-card-section class="q-pt-xl q-pl-xl col-4">
+          <div class="text-h6 text-weight-bold text-primary">{{dtInit}}</div>
+          <div class="text-body1">{{cargo}}</div>
+          <div class="text-body1">{{empresa}}</div>
         </q-card-section>
-
-          {{paragrafo}}
+        <q-card-section class="q-pa-xl">
+          <div class="text-body1">{{text}}</div>
+          <div class="text-body1">{{text2}}</div>
+        </q-card-section>
       </q-card-section>
     </q-card>
 </template>
 
 <script setup>
-const paragrafo = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-
 const props = defineProps({
-  dtInit: String
+  dtInit: String,
+  cargo: String,
+  empresa: String,
+  text: String,
+  text2: String
 })
 
 console.log(props.dtInit)
--- src/pages/CurriculoPage.vue
+++ src/pages/CurriculoPage.vue
@@ -1,13 +1,55 @@
 <template>
 
-    <div class="q-mt-lg">
+    <div class="q-mt-lg text-h1">
         <h1>Currículo</h1>
     </div>
-    <div >
+    <div class="q-pa-md row q-gutter-md justify-center">
+        <CardCurriculo
+            dtInit='set 2020 - set 2022'
+            cargo="Desenvolvedor Full-Stack"
+            empresa="Globalbot"
+            text="ATIVIDADES:
+            Responsável pela implementação de novas
+            features e correção de bugs, integração entre o
+            sistema da empresa com de terceiros para
+            automatização dos bots, manutenção das apis e
+            front-end."
+            text2="
+            TECNOLOGIAS UTILIZADAS
+            • NodeJS
+            • Express.JS
+            • MongoDb
+            • Vue.js
+            • Quasar
+            • CSS
+            • JavaScript"
+        />
+        <CardCurriculo
+            dtInit='fev 2020 - ago 2020'
+            cargo="Assistente de TI"
+            empresa="Academy Class"
+            text="ATIVIDADES:
+            Responsável pela manutenção dos sistemas
+            operacionais Linux, pela rede do call center."
+            text2="TECNOLOGIAS UTILIZADAS:
+            • Linux
+            • Roteador microtik
+            • Sistema operacional Windows
+            • Hardware"
+        />
+        <CardCurriculo
+            dtInit='mar 2018 - ago 2018'
+            cargo="Suporte técnico"
+            empresa="Softsystem Engenharia de software"
+            text="ATIVIDADE:
+            Suporte n1 e n2"
+            text2="TECNOLOGIAS UTILIZADAS
+            • Software cosmo
+            • Firebase
+            • SO Windows"
+        />
 
     </div>
-    <CardCurriculo  dtInit="" />
-
 </template>
 
 <script setup>

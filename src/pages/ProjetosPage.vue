fff95d8d156549c91986f47692d66b26c72f8ae9
Filipe <filipe.matias@globalbot.com.br>
2022-10-12 14:44:14 -0300

    Update ProjetosPage.vue

--- src/pages/ProjetosPage.vue
+++ src/pages/ProjetosPage.vue
@@ -1,18 +1,20 @@
 <template>
-    <div class="q-mt-lg">
-        <h1>Projetos</h1>
-    </div>
-    <div class="text-body1">Sou um parágrafo. Aqui você pode adicionar e editar seu próprio texto.
-        É fácil, basta clicar em "Editar texto" ou clicar duas vezes sobre mim.
-        Você também pode alterar a fonte e mais.
-        Sou um ótimo lugar para você compartilhar a sua história.
-    </div>
-    <div class="q-pa-md row q-gutter-md justify-center">
-        <CardProjetos
-            projectName='Site curriculo'
-            img="printsitecurriculo.png"
-            text="Site feito em vue.js e quasar, hospedado no serviço S3 da AWS"
-        />
+    <div class="fit column inline justify-center content-center">
+        <div class="q-mt-lg self-center">
+            <h1>Projetos</h1>
+            <div class="text-body1 my-description text-center">
+                Fala galera segue aqui alguns de meus projetos que desenvolvo para estudar e
+                para brincar com algumas tecnologias desse mundo dev,
+                abaixo tem uma breve descrição e o link para poder visualizar o código no github.
+            </div>
+        </div>
+        <div class="q-pa-md q-gutter-md ">
+            <CardProjetos
+                projectName='Site curriculo'
+                img="printsitecurriculo.png"
+                text="Site feito em vue.js e quasar, hospedado no serviço S3 da AWS"
+            />
+        </div>
     </div>
 
 </template>
@@ -22,5 +24,9 @@ import CardProjetos from '../components/CardProjetos.vue'
 </script>
 
 <style scoped>
+.my-description {
+    width: auto;
+    max-width: 580px;
+}
 
 </style>

235e79f30e8ffdd4d88b7677bf8b05c7f862fdee
Filipe Matias <filipematiasp@gmail.com>
2026-06-22 17:35:19 -0300

    fix:New url linkedin

--- src/pages/ContatoPage.vue
+++ src/pages/ContatoPage.vue
@@ -8,7 +8,7 @@
                     <div class="text-body1">Gostaria de entrar em contato comigo?</div>
                 </q-card-section>
                 <q-card-section class="row justify-center">
-                    <q-btn flat size="md" icon="fa-brands fa-linkedin-in" href="https://www.linkedin.com/in/filipe-matias-227151115/" target="_black"></q-btn>
+                    <q-btn flat size="md" icon="fa-brands fa-linkedin-in" href="https://www.linkedin.com/in/filipe-matias-pinto/" target="_black"></q-btn>
                     <q-btn flat size="md" icon="fa-brands fa-github" href="https://github.com/filipematiasp" target="_black"></q-btn>
                     <q-btn flat size="md" icon="fa-brands fa-whatsapp" href="https://api.whatsapp.com/send?phone=5548988258786" target="_black"></q-btn>
                 </q-card-section>
